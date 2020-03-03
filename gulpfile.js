e0d7bbdcd8dada6c5690809b5340607c8e213949
Lait <radroachjohny@gmail.com>
2020-03-03 11:44:15 +0300

    create .min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
  
 gulp.task('hello', function(done)  {
   console.log('Привет, мир!');
@@ -14,4 +16,11 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest('dist'));
 });
