311ae267116161edefc1065a51882959a34f7347
Lait <radroachjohny@gmail.com>
2020-03-04 15:47:49 +0300

    feat: create gulp sass in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
  
-gulp.task('hello', function(done)  {
-  console.log('Привет, мир!');
-  done();
-});
-
 // Static server
-gulp.task('browser-sync', function() {
+ function bs() {
+   serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.html").on('change', browserSync.reload);
+  };
 
-gulp.task('minify-css', () => {
-  return gulp.src('styles/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest('dist/css/'));
-});
+
+ function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("/css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+// gulp.task('minify-css', () => {
+//   return gulp.src('styles/*.css')
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(rename({suffix: ".min"}))
+//     .pipe(gulp.dest('dist/css/'));
+// });
