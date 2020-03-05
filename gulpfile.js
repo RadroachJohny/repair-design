1329e60969e386ef2f6d683b2d206137425fbdeb
Lait <radroachjohny@gmail.com>
2020-03-05 18:43:01 +0300

    feat: add _fonts.scss and 2 fonts. changde gulp file to watch scss files

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,39 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
  
 // Static server
- function bs() {
-   serveSass();
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    watch("./*.html").on('change', browserSync.reload);
-    watch("./sass/**/*.sass", serveSass);
-    watch("./js/*.html").on('change', browserSync.reload);
-  };
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.html").on('change', browserSync.reload);
+}
 
 
- function serveSass() {
-  return src("./sass/*.sass")
-      .pipe(sass())
-      .pipe(dest("/css"))
-      .pipe(browserSync.stream());
-};
+function serveSass() {
+return src("./sass/**/*.sass", "./sass/**/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(dest("/css"))
+    .pipe(browserSync.stream());
+}
 
 exports.serve = bs;
+
 // gulp.task('minify-css', () => {
 //   return gulp.src('styles/*.css')
 //     .pipe(cleanCSS({compatibility: 'ie8'}))
 //     .pipe(rename({suffix: ".min"}))
 //     .pipe(gulp.dest('dist/css/'));
 // });
+
