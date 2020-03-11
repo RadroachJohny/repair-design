42624274550b52cff43256d23f06c2a237d1a720
Lait <radroachjohny@gmail.com>
2020-03-11 21:50:11 +0300

    feat:add feat to close modal on esc and outside click

--- js/main.js
+++ js/main.js
@@ -4,11 +4,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
+  };
   
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   closeBtn.addEventListener('click', switchModal);
+
+    window.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        modal.classList.toggle('modal--visible');
+      }
+    });
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.classList.toggle('modal--visible');
+      }
+    };
 });
+
+
+
+
