3a2117be30af28cfa0ba0854c9fdfc14c0bf357a
Lait <radroachjohny@gmail.com>
2020-03-20 10:39:09 +0300

    .]

--- js/main.js
+++ js/main.js
@@ -278,6 +278,48 @@ $('.control__form').validate({
 $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "Ваш номер телефона"} );
 
 
+
+
+
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.754117, 37.623421],
+          behaviors: ['drag', 'dblClickZoom'],
+          zoom: 9
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Наш офис',
+          balloonContent: 'Вход со двора'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/marker.png',
+          // Размеры метки.
+          iconImageSize: [32, 32],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      });
+
+     
+
+  myMap.geoObjects
+      .add(myPlacemark);
+});
+
+
+
 });
 
 
@@ -319,3 +361,5 @@ var btn = $('#button');
     $('html, body').animate({scrollTop:0}, 800);
     });
   });
+
+
